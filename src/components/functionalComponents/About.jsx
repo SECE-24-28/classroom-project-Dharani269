4abcba02d171f0ccc53fb22e1bbd882ddd8900c0
Dharani269 <dharani.m2024csecs@sece.ac.in>
2025-12-17 09:49:15 +0530

    DAY 9-11

--- src/components/functionalComponents/About.jsx
+++ src/components/functionalComponents/About.jsx
@@ -1,16 +1,6 @@
-import { Link } from 'react-router-dom';
-
 const About = () => {
     return (
         <div>
-            <nav>
-                <ul>
-                    <li><Link to="/home">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/learning-react">Learning React</Link></li>
-                </ul>
-            </nav>
             <h1>About</h1>
             <p>This is About Page</p>
             <hr />
